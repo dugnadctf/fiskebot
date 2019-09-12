5fa29a44e7159f8fa9289e8c26c2996db29effb1
Henry Wang <wanghenry1998@gmail.com>
2019-09-12 15:04:53 -0500

    Fix delete challenges

--- cogs/ctfmodel.py
+++ cogs/ctfmodel.py
@@ -260,7 +260,7 @@ class CtfTeam(object):
         fullname = f'{self.name}-{name}'
         chal = self.find_chal(name)
         chk_upd(fullname, teams.update_one({'chan_id': cid}, 
-            {'$pull': {'chals': chal['chan_id']}}))
+            {'$pull': {'chals': chal.chan_id}}))
         await chal._delete(catg_archive)
         self.refresh()
 
@@ -436,6 +436,7 @@ class Challenge(object):
 
     async def _delete(self, catg_archive):
         cid = self.__id
+        guild = self.__guild
 
         # Delete entry
         chk_del(self.name, self.__chals.delete_one({'chan_id': self.__id}))
