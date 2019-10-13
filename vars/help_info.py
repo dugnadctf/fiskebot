cd02e5251e4a48c5c392df696c2172bfba1e00e4
SmellyCharlie <3658646-SmellyCharlie@users.noreply.gitlab.com>
2019-10-13 22:20:05 +0000

    Docs and samples

--- vars/help_info.py
+++ vars/help_info.py
@@ -1,39 +1,45 @@
-import requests
-import re
-import random
 import discord
-from string import *
 
-help_page = '''
+
+src = "https://github.com/NullPxl/NullCTF"
+src_fork = "https://gitlab.com/inequationgroup/igCTF"
+creator_info = """https://blog.inequationgroup.com/
+https://gitlab.com/inequationgroup
+https://ctftime.org/team/59772"""
+
+help_page = """
 Adapted from: https://github.com/NullPxl/NullCTF
 
-`!ctftime <upcoming/current> <number>`
-return info on a number of upcoming ctfs, or currently running ctfs from ctftime.org (number param is just for upcoming)
+`!ctftime <current/upcoming <number>>`
+Returns info on ongoing CTFs from ctftime.org, or displays the `number` of upcoming events.
 
 `!ctftime <countdown/timeleft>`
-return specific times for the time until a ctf begins, or until a currently running ctf ends.
+Returns remaining time until an upcoming CTF begins, or ongoing event ends.
 
 `!ctftime top <year>`
-display the leaderboards from ctftime from a certain year.
+Display the leader boards from ctftimeorg for a specified `year`.
 
 `!create_ctf "<ctf name>"`
-create a text channel and role in the CTF category for a ctf (must have permissions to manage channels).
+Create a text channel and role in the CTF category for a specified `ctf name`.
+(This requires the bot has manage channels permissions)
 
 `!ctf <action>...`
-Does various actions in a CTF team context. You can issue these commands in a channel that was created by the `!create_ctf` command. See `!ctf help` for more details.
+You can only issue these commands in a channel that was created by the `!create_ctf` command.
+See `!ctf help` for more details.
 
 `!chal <action>...`
-Does various actions in a challenge context. You can issue these commands in a channel that was created by the `!ctf add` command. See `!chal help` for more details.
+You can only issue these commands in a channel that was created by the `!ctf add` command. 
+See `!chal help` for more details.
 
 `!htb`
-return the latest tweet from @hackthebox_eu that says when the next box will be released
+return the latest tweet from @hackthebox_eu that says when the next box will be released.
 
 *next page is utility commands*
 
 **page: 1/2 - (!help 1)**
-'''
+"""
 
-help_page_2 = '''
+help_page_2 = """
 
 
 `!rot <message> <direction(optional, will default to left)>`
@@ -58,7 +64,7 @@ encode or decode based on url encoding - if message has spaces use quotations
 reverse the supplied string - if message has spaces use quotations
 
 `!counteach <message>`
-count the occurences of each character in the supplied message - if message has spaces use quotations
+count the occurrences of each character in the supplied message - if message has spaces use quotations
 
 `!characters <message>`
 count the amount of characters in your supplied message
@@ -85,52 +91,50 @@ for the truth
 report an issue you found with the bot, if it is helpful your name will be added to the 'cool names' list!
 
 **page: 2/2 - (!help 2)** ; more commands and documentation viewable on the github page (>source)
-'''
+"""
 
-ctf_help_text = '''
+ctf_help_text = """
 These commands are callable from a CTF **team** channel environment.
 
-`!ctf working <chal>`
-Mark that you are working on this challenge. You will also be invited to the respective private channel 
+`!ctf add "<challenge>"`
+Add a `challenge` and a respective private channel. Challenge names may be altered to meet Discord restrictions.
+(i.e. no special characters, less than 32 characters long, etc...)
+
+`!ctf working <challenge>`
+Mark that you are working on a `challenge`. You will also be invited to the respective private channel. 
 
 `!ctf <join/leave>`
 Gets/gets rid of the CTF role created with this CTF team.
 
 `!ctf invite <user>`
-Invites a user to CTF team. (Basically the user gets the CTF role)
+Invites a user to CTF team - `user` is granted the CTF role.
 
-`!ctf add "<chal>"`
-Add a challenge and a respective private channel. There are certain restrictions on the challenge name (i.e. no special characters, less than 32 characters long, etc...)
-
-`!ctf delete "<chal>"`
-Remove a challenge (must be able to manage channels). This will NOT automatically delete the respective private channel (if deemed necessary, an admin will manually delete it).
+`!ctf delete "<challenge>"`
+Remove a challenge (this requires the bot has manage channels permissions). 
+This will **not** automatically delete the respective private channel. Server staff can remove manually if required.
 
 `!ctf archive`
-Archives this ctf and all the respective challenges (must be able to manage channels)
+Archives this ctf and all the respective challenges (this requires the bot has manage channels permissions). 
 
 `!ctf unarchive`
-Unarchives this ctf and all the respective challenges (must be able to manage channels)
+Unarchives this ctf and all the respective challenges (this requires the bot has manage channels permissions). 
 
-'''
+"""
 
-chal_help_text = '''
+chal_help_text = """
 These commands are callable from a CTF **challenge** environment.
 
-`!chal done [<with_users...>]`
-Marks this challenge as completed. You may optionally include @'s of users that worked with you. Once a challenge is completed, **no** one except you (and admins) can tamper with the done list or change it to "undone". This will also move the channel to the "done" category.
+`!chal done [<users>]`
+Marks this challenge as completed, and moves channel to "done" category. You may optionally include @'s of `users` that worked with you. 
+Once a challenge is completed, **no one** except you (and admins) can alter the done list or change reset the status to "undone". 
 
 `!chal invite <user>`
-Invites a user to a challenge channel. 
+Invites a `user` to a challenge channel. 
 
 `!chal undone`
 Marks this challenge as **not** completed. This will move the channel back to the "working" category.
 
-'''
-
-# TODO: update it
-src_fork = "https://github.com/theKidOfArcrania/UTCCtfBot"
-src = "https://github.com/NullPxl/NullCTF"
-creator_info = "https://youtube.com/nullpxl\nhttps://github.com/nullpxl\nhttps://twitter.com/nullpxl"
+"""
 
 
 async def embed_help(chan, help_topic, help_text):
