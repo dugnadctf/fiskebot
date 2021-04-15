f7a30f8037fcddb2f7e0cc0d8c763f1f4ac288dc
Kasper Furnes Bernhoff <kfbi@protonmail.com>
2021-04-16 01:11:59 +0200

    3rd times the charm

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,17 @@
 import os
 
+
+def parse_variable(variable, default=None):
+    value = os.getenv(variable, None)
+    if isinstance(default, int):
+        return int(value) if value and value.isdigit() else default
+    else:
+        return value if value and value != "" else default
+
+
 config = {
     # Connection to mongo backend database
-    "db": os.getenv("MONGODB_URI", "mongodb://mongo:27017"),
+    "db": parse_variable("MONGODB_URI", "mongodb://mongo:27017"),
     # Bot token.
     # Follow the wikihow guide for instructions:
     # https://www.wikihow.com/Create-a-Bot-in-Discord#Creating-the-Bot-on-Discord
@@ -14,52 +23,47 @@ config = {
     # On the same site you can change the name and profile picture of the bot
     # -
     # The token should have a similar format to the one bellow
-    "token": "NjM3NTU5ODU2NTA4OTYwODA2.XbP74w.k7yIIBzxtVInfQHw6QdIGu97Jgg",
+    "token": parse_variable("DISCORD_TOKEN"),
     # To invite the bot, use a link similar to:
     # https://discord.com/oauth2/authorize?&client_id=000000000000000001&scope=bot&permissions=8
     # Replace the client_id with your client id. permissions=8 means the manage permission which this bot needs to manage the channels.
     #
     # The level of logging for alerting, etc. Uses the logging levels specified here: https://docs.python.org/3/library/logging.html
-    "logging_level": os.getenv("LOGGING_LEVEL", "INFO"),
+    "logging_level": parse_variable("LOGGING_LEVEL", "INFO"),
     # Logging format, see https://docs.python.org/3/library/logging.html#formatter-objects
-    "logging_format": os.getenv(
+    "logging_format": parse_variable(
         "LOGGING_FORMAT", "%(asctime)s:%(levelname)s:%(name)s: %(message)s"
     ),
     # If enabled, will send logging to this file
-    "logging_file": os.getenv("LOGGING_FILE", None),
+    "logging_file": parse_variable("LOGGING_FILE"),
     # The minimum level for logging into the logging channel (`CHANNEL_LOGGING`)
-    "logging_discord_level": os.getenv("LOGGING_DISCORD_LEVEL", "ERROR"),
+    "logging_discord_level": parse_variable("LOGGING_DISCORD_LEVEL", "ERROR"),
     # Prefix for all the bot commands
-    "prefix": os.getenv("COMMAND_PREFIX", "!"),
+    "prefix": parse_variable("COMMAND_PREFIX", "!"),
     # Profile ids of the maintainers of your installation. These will be messaged when running `!report "issue"` or `!request "feature"`
     # These people also have permission to export and delete ctfs.
     "maintainers": [
         int(maintainer)
-        for maintainer in os.getenv("MAINTAINERS", "").replace(" ", "").split(",")
+        for maintainer in parse_variable("MAINTAINERS", "").replace(" ", "").split(",")
         if maintainer.isdigit()
-    ],
-    "guild_ids": [
-        int(guild_id)
-        for guild_id in os.getenv("GUILD_IDS", "").replace(" ", "").split(",")
-        if guild_id.isdigit()
     ],
     "categories": {
         # Category where channels for challenges that are currently being worked on during a CTF
-        "working": os.getenv("CATEGORY_WORKING", "working"),
+        "working": parse_variable("CATEGORY_WORKING", "working"),
         # Category where channels for challenges that are marked as done during a CTF
-        "done": os.getenv("CATEGORY_DONE", "done"),
+        "done": parse_variable("CATEGORY_DONE", "done"),
         # Category to move channels to when the CTF is over. There is a max limit on 50 channels per category. The bot will automatically move channels to new categories when needed.
-        "archive-prefix": os.getenv("CATEGORY_ARCHIVE_PREFIX", "archive"),
+        "archive-prefix": parse_variable("CATEGORY_ARCHIVE_PREFIX", "archive"),
     },
     "channels": {
         # The channel to upload exports to
-        "export": os.getenv("CHANNEL_EXPORT", "export"),
+        "export": parse_variable("CHANNEL_EXPORT", "export"),
         # If enabled, will send logging to this channel, based on the `LOGGING_DISCORD_LEVEL` logging level
-        "logging": os.getenv("CHANNEL_LOGGING", None),
+        "logging": parse_variable("CHANNEL_LOGGING"),
     },
     # CTFtime id for the default team to lookup using the `!ctftime team` command
     "team": {
-        "id": os.getenv("CTFTIME_TEAM_ID", -1),
-        "name": os.getenv("CTFTIME_TEAM_NAME", None),
+        "id": parse_variable("CTFTIME_TEAM_ID", -1),
+        "name": parse_variable("CTFTIME_TEAM_NAME"),
     },
 }
